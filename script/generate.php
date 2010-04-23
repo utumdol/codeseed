e20969a9ffc3ec23a4e189857f881a0f34ef05e6
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-04-23 09:11:56 +0000

    Split generate.php to generate_model.php and generate_controller.php. But it didn't work yet.
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@11 cc3e2240-c383-0410-974a-c2160eca7f04

--- script/generate.php
+++ script/generate.php
@@ -2,25 +2,40 @@
 // include system init 
 require_once(dirname(__FILE__) . '/../config/environment.php');
 
-if ($argc < 2) {
-	echonl("Usage:");
-	echonl("1. php script/generate model model_name");
-	echonl("2. php script/generate controller controller_name action_name_1 action_name_2 ...");
+if ($argc < 3) {
+	echonl("Example:");
+	if (empty($argv[1]) || $argv[1] == 'model') {
+		echonl("\t`php script/generate.php model user`");
+	}
+	if (empty($argv[1]) || $argv[1] == 'controller') {
+		echonl("\t`php script/generate.php controller login login logout`");
+	}
 	exit(0);
 }
 
 switch($argv[1]) {
 	case 'model':
-		echonl("I'will make model");
+		if (file_exists(MODEL_DIR)) {
+			echonl("\texist app/models");
+		}
+
+		if (file_exists(filename_to_classname($argv[2]))) {
+			echonl("\texist app/models");
+		}
+		
 		// 1. mkdir app
-		// 1. mkdir app/model
-		// 2. generate model file
-		// 3. mkdir db
-		// 4. mkdir db/migrate
-		// 5. generate model file
+		// 2. mkdir app/model
+		// 3. generate model file
+		// 4. mkdir db
+		// 5. mkdir db/migrate
+		// 6. generate model file
 		break;
 	case 'controller':
 		echonl("I'will make controller");
+		if (file_exists(CNTR_DIR)) {
+			echonl("\texist app/controller");
+		}
+
 		// 1. mkdir app
 		// 2. mkdir app/controller
 		// 3. generate controller file with actions
