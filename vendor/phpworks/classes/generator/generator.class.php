13a4817fb32e701f912b09f085d6fdbdc56cb688
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-06-24 14:32:19 +0000

    Fix migration_generator.class.php to use generator super class
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@41 cc3e2240-c383-0410-974a-c2160eca7f04

--- vendor/phpworks/classes/generator/generator.class.php
+++ vendor/phpworks/classes/generator/generator.class.php
@@ -1,18 +1,8 @@
 <?php
 class Generator {
 	private $name = '';
-	private $path = MIGR_DIR;
-	private $template = '<?php
-class Create<CLASSNAME> extends Migration {
-	public function up() {
-		$this->create_table(\'<TABLENAME>\');
-	}
-
-	public function down() {
-		$this->drop_table(\'<TABLENAME>\');
-	}
-}
-?>';
+	private $path = '';
+	private $template = '';
 
 	public function Generator($name) {
 		$this->name = $name;
@@ -29,17 +19,11 @@ class Create<CLASSNAME> extends Migration {
 	}
 
 	private function getFileName() {
-		$tablename = filename_to_tablename($this->name);
-		$this_time = date('YmdHis');
-		return $this_time . '_create_' . $tablename . '.class.php';
+		return $this->name;
 	}
 
 	private function getContents() {
-		$tablename = filename_to_tablename($this->name);
-		$classname = tablename_to_classname($this->name);
-		$result = str_replace('<TABLENAME>', $tablename, $this->template);
-		$result = str_replace('<CLASSNAME>', $classname, $result);
-		return $result;
+		return $this->template;
 	}
 
 	private function generatePath() {
--- vendor/phpworks/classes/generator/migration_generator.class.php
+++ vendor/phpworks/classes/generator/migration_generator.class.php
@@ -14,33 +14,12 @@ class Create<CLASSNAME> extends Migration {
 }
 ?>';
 
-	/*
-	public function MigrationGenerator($name) {
-		$this->name = $name;
-	}
-	*/
-
-	/*
-	public function generate() {
-		$this->validation();
-		$this->generatePath();
-		$this->generateFile();
-	}
-	*/
-
-	/*
-	private function validation() {
-		return true;
-	}
-	*/
-
 	private function getFileName() {
 		$tablename = filename_to_tablename($this->name);
 		$this_time = date('YmdHis');
 		return $this_time . '_create_' . $tablename . '.class.php';
 	}
 
-	/*
 	private function getContents() {
 		$tablename = filename_to_tablename($this->name);
 		$classname = tablename_to_classname($this->name);
@@ -48,28 +27,5 @@ class Create<CLASSNAME> extends Migration {
 		$result = str_replace('<CLASSNAME>', $classname, $result);
 		return $result;
 	}
-	*/
-
-	/*
-	private function generatePath() {
-		if (is_dir($this->path)) {
-			echonl('exist ' . $this->path);
-		} else {
-			echonl('generate ' . $this->path);
-			mkdir($this->path, 0755, true);
-		}
-	}
-	*/
-
-	/*
-	private function generateFile() {
-		$filename = $this->getFileName();
-		$content = $this->getContents();
-		echonl('generate ' . $this->path . '/' . $filename);
-		$f = fopen($this->path . '/' . $filename, 'w');
-		fwrite($f, $content, strlen($content)); 
-		fclose($f);
-	}
-	*/
 }
 ?>
