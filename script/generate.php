e8013dbf734f6e30567b8ce5c65a0557ecd294b3
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-06-25 14:30:53 +0000

    [#6725] fix generate.php script
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@45 cc3e2240-c383-0410-974a-c2160eca7f04

--- script/generate.php
+++ script/generate.php
@@ -2,18 +2,24 @@
 // include system init 
 require_once(dirname(__FILE__) . '/../config/environment.php');
 
+//validation if there are enough parameters
 if ($argc < 3) {
 	echonl("Example:");
-	if (empty($argv[1]) || $argv[1] == 'model') {
-		echonl("\t`php script/generate.php model user`");
-	}
-	if (empty($argv[1]) || $argv[1] == 'controller') {
-		echonl("\t`php script/generate.php controller login login logout`");
-	}
+	echonl("\t`php script/generate.php model user`");
+	echonl("\t`php script/generate.php migration add_price`");
+	echonl("\t`php script/generate.php controller login login logout`");
 	exit(0);
 }
 
-$generator = new MigrationGenerator($argv[2]);
+// validation if there is generator class file
+$generator_name = $argv[1];
+if (!file_exists(SYS_CLASSES . '/generator/' . $generator_name . '_generator.class.php')) {
+	echonl("No " . $generator_name . ' generator exist.');
+	exit(0);
+}
+
+$generator_classname = filename_to_classname($argv[1]) . 'Generator';
+$generator = new $generator_classname($argv[2]);
 $generator->generate();
 
 /*
