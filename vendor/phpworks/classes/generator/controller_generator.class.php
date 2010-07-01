1dabe0264ccf28e52ade713a78ebbbba528805c3
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-07-01 14:52:49 +0000

    [#8416] make controller generator
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@54 cc3e2240-c383-0410-974a-c2160eca7f04

--- vendor/phpworks/classes/generator/controller_generator.class.php
+++ vendor/phpworks/classes/generator/controller_generator.class.php
@@ -1,34 +1,47 @@
 <?php
 class ControllerGenerator extends Generator {
 	var $name = '';
+	var $functions = '';
+
 	var $path = CONT_DIR;
 	var $template = '<?php
-class <CLASSNAME> extends Controller {
-	<METHODS>
+class <class> extends Controller {
+	<functions>
 }
 ?>';
-	var $method_template = 'public function <METHOD> {
-	}';
+	var $function_template = 'public function <function> {
+	}
+	
+	';
 
-	public function ControllerGenerator($name, $methods = array()) {
+	public function ControllerGenerator($name, $functions = array()) {
+		$this->name = $name;
+		$this->functions = $functions;
 	}
 
 	public function generate() {
 		$this->validation();
 		$this->generatePath();
 		$this->generateFile();
-
-		$migration_generator = new MigrationGenerator($this->name);
-		$migration_generator->generate();
 	}
 
 	public function getFileName() {
 		return $this->name . '.class.php';
 	}
 
+	public function getFunctionsContents() {
+		$result = '';
+		foreach($this->functions as $function) {
+			$result .= str_replace('<function>', $function, $this->function_template);
+		}
+		return $result;
+	}
+
 	public function getContents() {
-		$classname = tablename_to_classname($this->name);
-		$result = str_replace('<CLASSNAME>', $classname, $this->template);
+		$class = tablename_to_classname($this->name);
+		$functions = $this->getFunctionsContents();
+		$result = str_replace('<class>', $class, $this->template);
+		$result = str_replace('<functions>', $functions, $result);
 		return $result;
 	}
 }
