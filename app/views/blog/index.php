7aa7a60b3d4eb27bb592866d3c4eb024a43e2cda
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-07-05 15:10:07 +0000

    [#8466] Oops, I have missed list view file index.php
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@69 cc3e2240-c383-0410-974a-c2160eca7f04

---
+++ app/views/blog/index.php
@@ -0,0 +1,31 @@
+<table>
+<thead>
+<tr>
+	<th>id</th>
+	<th>subject</th>
+	<th>update</th>
+	<th>delete</th>
+</tr>
+</thead>
+<tbody>
+<?php
+foreach($this->list as $article) {
+?>
+<tr>
+	<td><a href="/user/view/<?= $article->id ?>"><?= $article->id ?></a></td>
+	<td><a href="/user/view/<?= $article->id ?>"><?= $article->subject ?></a></td>
+	<td><a href="/user/update_form/<?= $article->id ?>">+</a></td>
+	<td><a href="/user/remove/<?= $article->id ?>" onclick="return confirm('정말 삭제하시겠습니까?');">-</a></td>
+</tr>
+<?php
+}
+?>
+</tbody>
+</table>
+<?
+$this->load_view('layout/paging');
+?>
+<div class="menu">
+<span>[<a href="/blog/index">처음으로</a>]</span><span>[<a href="/blog/post_form">등록하기</a>]</span>
+</div>
+
