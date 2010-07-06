46778b1634762bcd3c016540292151ca27f6e818
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-07-06 13:09:12 +0000

    [#8466] move update and delete link to view
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@75 cc3e2240-c383-0410-974a-c2160eca7f04

--- app/views/blog/index.php
+++ app/views/blog/index.php
@@ -3,8 +3,6 @@
 <tr>
 	<th>id</th>
 	<th>subject</th>
-	<th>update</th>
-	<th>delete</th>
 </tr>
 </thead>
 <tbody>
@@ -14,8 +12,6 @@ foreach($this->list as $article) {
 <tr>
 	<td><a href="/blog/view/<?= $article->id ?>"><?= $article->id ?></a></td>
 	<td><a href="/blog/view/<?= $article->id ?>"><?= $article->subject ?></a></td>
-	<td><a href="/blog/update_form/<?= $article->id ?>">+</a></td>
-	<td><a href="/blog/remove/<?= $article->id ?>" onclick="return confirm('정말 삭제하시겠습니까?');">-</a></td>
 </tr>
 <?php
 }
@@ -26,6 +22,6 @@ foreach($this->list as $article) {
 $this->load_view('layout/_paging');
 ?>
 <div class="menu">
-<span>[<a href="/blog/index">처음으로</a>]</span><span>[<a href="/blog/post_form">등록하기</a>]</span>
+	<span>[<a href="/blog/index">처음으로</a>]</span><span>[<a href="/blog/post_form">등록하기</a>]</span>
 </div>
 
--- app/views/blog/view.php
+++ app/views/blog/view.php
@@ -5,6 +5,8 @@
 	<span class="content"><?= nl2br($this->article->content) ?></span>
 </div>
 <div class="menu">
-<span>[<a href="/blog/index">목록으로</a>]</span>
+	<span>[<a href="/blog/index">목록으로</a>]</span>
+	<span>[<a href="/blog/update_form/<?= $this->article->id ?>">수정하기</a>]</span>
+	<span>[<a href="/blog/remove/<?= $this->article->id ?>" onclick="return confirm('정말 삭제하시겠습니까?');">삭제하기</a>]</span>
 </div>
 
