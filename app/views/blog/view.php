252966cc48cb8037b620fcf5624d18e572f5600e
utumdol <utumdol@cc3e2240-c383-0410-974a-c2160eca7f04>
2010-07-07 13:14:55 +0000

    [#8466] amend css of view mode
    git-svn-id: https://dev.naver.com/svn/phpworks/trunk@79 cc3e2240-c383-0410-974a-c2160eca7f04

--- app/views/blog/view.php
+++ app/views/blog/view.php
@@ -1,10 +1,8 @@
-<div>
-	<span class="subject"><?= $this->article->subject ?></span>
+<div id="view">
+	<div id="subject"><?= $this->article->subject ?></div>
+	<div id="content"><?= nl2br($this->article->content) ?></div>
 </div>
-<div>
-	<span class="content"><?= nl2br($this->article->content) ?></span>
-</div>
-<div class="menu">
+<div class="menu_area">
 	<span>[<a href="/blog/index">목록으로</a>]</span>
 	<span>[<a href="/blog/update_form/<?= $this->article->id ?>">수정하기</a>]</span>
 	<span>[<a href="/blog/remove/<?= $this->article->id ?>" onclick="return confirm('정말 삭제하시겠습니까?');">삭제하기</a>]</span>
