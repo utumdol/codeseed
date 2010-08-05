0aac22c78931bf02c0892cd49f39b49ad2fe1e06
utumdol <utumdol@naver.com>
2010-08-06 01:09:32 +0900

    remove database info from migration. closes #16.

--- vendor/phpworks/classes/base/migration.class.php
+++ vendor/phpworks/classes/base/migration.class.php
@@ -1,35 +1,41 @@
 <?php
 class Migration {
 
-	public function Migration() {
+	public function create_table($table_name) {
 		global $DATABASE;
 
-		$this->database = $DATABASE;
-	}
-
-	public function create_table($table_name) {
-		$this->database->execute("CREATE TABLE $table_name (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
+		$DATABASE->execute("CREATE TABLE $table_name (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id))");
 	}
 
 	public function drop_table($table_name) {
-		$this->database->execute("DROP TABLE $table_name");
+		global $DATABASE;
+
+		$DATABASE->execute("DROP TABLE $table_name");
 	}
 
 	public function add_column($table_name, $name, $type, $size, $is_null = true) {
+		global $DATABASE;
+
 		$not_null = ($is_null) ? '' : 'NOT NULL'; 
-		$this->database->execute("ALTER TABLE $table_name ADD COLUMN $name $type($size) $not_null");
+		$DATABASE->execute("ALTER TABLE $table_name ADD COLUMN $name $type($size) $not_null");
 	}
 
 	public function drop_column($table_name, $name) {
-		$this->database->execute("ALTER TABLE $table_name DROP COLUMN $name");
+		global $DATABASE;
+
+		$DATABASE->execute("ALTER TABLE $table_name DROP COLUMN $name");
 	}
 
 	public function add_index($table_name, $name, $fields) {
-		$this->database->execute("ALTER TABLE $table_name ADD INDEX $name ($fields)");
+		global $DATABASE;
+
+		$DATABASE->execute("ALTER TABLE $table_name ADD INDEX $name ($fields)");
 	}
 
 	public function drop_index($table_name, $name) {
-		$this->database->execute("ALTER TABLE $table_name DROP INDEX $name");
+		global $DATABASE;
+
+		$DATABASE->execute("ALTER TABLE $table_name DROP INDEX $name");
 	}
 }
 
