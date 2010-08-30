ebbdf8b3f97745b469042319b5d70627ad69a49a
utumdol <utumdol@naver.com>
2010-08-30 23:13:20 +0900

    arrange some files

--- config/database.php
+++ config/database.php
@@ -1,8 +1,14 @@
 <?php
+
+// for development
 define('DBMS', 'mysql');
 define('DBHOST', 'localhost');
 define('DBPORT', '3306');
 define('DBUSER', 'root');
 define('DBPASSWD', '');
 define('DBNAME', 'phpworks');
+
+// for teast
+
+// for production
 
