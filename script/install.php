88e3761b219afa07d91c9d279d3bfb9b6832c966
utumdol <utumdol@naver.com>
2010-09-13 08:33:50 +0900

    add install features

---
+++ script/install.php
@@ -0,0 +1,23 @@
+<?php
+// include system init 
+require_once(dirname(__FILE__) . '/../config/environment.php');
+
+// connect db connection
+$db = get_db();
+$db->connect();
+
+// init table
+$tables = $db->get_tables();
+
+// init sessions table
+if (!in_array('sessions', $tables)) {
+	$migration = new CreateSessions();
+	$migration->up();
+}
+
+// init version table
+if (!in_array('schema_version', $tables)) {
+	$migration = new CreateSchemaVersion();
+	$migration->up();
+}
+
--- script/migrate.php
+++ script/migrate.php
@@ -13,22 +13,8 @@ function print_migration_status($direction, $classname, $version) {
 }
 
 // connect db connection
+$db = get_db();
 $db->connect();
-
-// init table
-$tables = $db->get_tables();
-
-// init sessions table
-if (!in_array('sessions', $tables)) {
-	$migration = new CreateSessions();
-	$migration->up();
-}
-
-// init version table
-if (!in_array('schema_version', $tables)) {
-	$migration = new CreateSchemaVersion();
-	$migration->up();
-}
 
 $schema_version = new SchemaVersion();
 if ($schema_version->count() == 0) {
