d460a9b1528fa7b3ff0aa4fd1400eaf09ad29d57
utumdol <utumdol@naver.com>
2010-10-09 13:50:08 +0900

    change some inline javascripts to head

--- app/views/user/register_form.php
+++ app/views/user/register_form.php
@@ -1,9 +1,11 @@
 <script type="text/javascript">
-// local scripts
 $(function() {
 	$('#email').keyup(function() {
 		$('#nickname').val($('#email').val().split('@')[0]);
 	})
+	$('#button_cancel').click(function() {
+		history.back();
+	});
 });
 </script>
 <form class="form" action="/user/register" method="post">
@@ -11,8 +13,8 @@ $(function() {
 $this->load_view('/user/_form');
 ?>
 <div class="button_area">
-	<span><input type="submit" value="등록하기" /></span>
-	<span><input type="button" value="취소하기" onclick="javascript:history.back()" /></span>
+	<span><input type="submit" id="button_register" value="등록하기" /></span>
+	<span><input type="button" id="button_cancel" value="취소하기" /></span>
 </div>
 </form>
 
