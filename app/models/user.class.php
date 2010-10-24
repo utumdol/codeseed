03370911f787d2aed2fd7bcb2960c6ce5f15b397
utumdol <utumdol@naver.com>
2010-10-24 21:22:23 +0900

    user register validation and blog form

--- app/models/user.class.php
+++ app/models/user.class.php
@@ -38,6 +38,16 @@ class User extends Model {
 			return false;
 		}
 
+		if ($this->count("email = '{$this->email}'") > 0) {
+			$this->errors->add('이미 등록되어 있는 이메일 주소입니다.');
+			return false;
+		}
+
+		if ($this->count("nickname = '{$this->nickname}'") > 0) {
+			$this->errors->add('동일한 별명이 이미 사용되고 있습니다. 다른 별명을 입력해 주세요.');
+			return false;
+		}
+
 		return true;
 	}
 }
--- app/views/blog/_form.php
+++ app/views/blog/_form.php
@@ -1,7 +1,9 @@
-<div>
-	<span><input type="text" id="subject" name="article[subject]" value="<?= h(get_value($this, array('article', 'subject'))) ?>" /></span>
+<div class="form_row form_top">
+	<div class="form_subject">제목</div>
+	<div class="input_area"><input type="text" class="input" id="subject" name="article[subject]" value="<?= h(get_value($this, array('article', 'subject'))) ?>" /></div>
 </div>
-<div>
-	<span><textarea id="content" name="article[content]"><?= h(get_value($this, array('article', 'content'))) ?></textarea></span>
+<div class="form_row">
+	<div class="form_subject">내용</div>
+	<div class="input_area"><textarea class="input" rows="20" id="content" name="article[content]"><?= h(get_value($this, array('article', 'content'))) ?></textarea></div>
 </div>
 
