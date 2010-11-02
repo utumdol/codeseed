07cf86520789b49c01d11c68f9e509a703a1ed6f
utumdol <utumdol@naver.com>
2010-11-02 23:57:22 +0900

    wide blog post form

--- app/views/blog/_form.php
+++ app/views/blog/_form.php
@@ -1,9 +1,7 @@
 <div class="form_row form_top">
-	<div class="form_subject">제목</div>
-	<div class="input_area"><input type="text" class="input" id="subject" name="article[subject]" value="<?= h(get_value($this, array('article', 'subject'))) ?>" /></div>
+	<div class="input_area" style="width: 100%;"><input type="text" class="input" id="subject" name="article[subject]" value="<?= h(get_value($this, array('article', 'subject'))) ?>" /></div>
 </div>
 <div class="form_row">
-	<div class="form_subject">내용</div>
-	<div class="input_area"><textarea class="input" rows="20" id="content" name="article[content]"><?= h(get_value($this, array('article', 'content'))) ?></textarea></div>
+	<div class="input_area" style="width: 100%;"><textarea class="input" rows="20" id="content" name="article[content]"><?= h(get_value($this, array('article', 'content'))) ?></textarea></div>
 </div>
 
