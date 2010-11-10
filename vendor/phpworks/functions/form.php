b0c953778c385fab4b3cc53ac6c616d5e9626c17
utumdol <utumdol@naver.com>
2010-11-10 22:18:29 +0900

    reserve only old parameter in the session. it reduce the session repository

--- vendor/phpworks/functions/form.php
+++ vendor/phpworks/functions/form.php
@@ -1,31 +1,37 @@
 <?php
-function has_property($obj, $prop) {
-	$arr = get_object_vars($obj);
-	return array_key_exists($prop, $arr);
-}
-
 function get_value($obj, $props = array()) {
 	global $flash;
+
+	if (empty($obj) || empty($props)) {
+		return null;
+	}
 
 	if (!is_array($props)) {
 		$props = array($props);
 	}
 
-	if (!has_property($obj, $props[0])) {
-		$obj = unserialize(serialize($flash->get('that')));
-	}
-
-	if (empty($obj)) {
-		return null;
+	if (!property_exists($obj, $props[0])) {
+		return get_value_from_array($flash->get('params'), $props);
 	}
 
 	foreach($props as $prop) {
-		if (has_property($obj, $prop)) {
+		if (property_exists($obj, $prop)) {
 			$obj = $obj->$prop;
 		} else {
 			return null;
 		}
 	}
 	return $obj;
+}
+
+function get_value_from_array($arr, $props = array()) {
+	foreach($props as $prop) {
+		if (array_key_exists($prop, $arr)) {
+			$arr = $arr[$prop];
+		} else {
+			return null;
+		}
+	}
+	return $arr;
 }
 
