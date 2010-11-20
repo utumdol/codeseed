9954544b4485517d0ec3d324bc42bc1953784306
utumdol <utumdol@naver.com>
2010-11-20 11:54:16 +0900

    enable to read articles without login

--- public/phpworks.php
+++ public/phpworks.php
@@ -25,8 +25,10 @@ $flash->load();
 
 // routing
 $path = parse_request_uri($_SERVER['PATH_INFO']);
-$controller_path = empty($path[1]) ? DEFAULT_CONTROLLER : $path[1];
-$action_path = empty($path[2]) ? DEFAULT_ACTION : $path[2];
+if (empty($path[1])) { $path[1] = DEFAULT_CONTROLLER; }
+if (empty($path[2])) { $path[2] = DEFAULT_ACTION; }
+$controller_path = $path[1];
+$action_path = $path[2];
 require_once(HELP_DIR. '/' . $controller_path . '.php');
 require_once(CNTR_DIR . '/' . $controller_path . '_controller.class.php');
 $controller_name = filename_to_classname($controller_path . '_controller');
