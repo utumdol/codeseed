f62f6acc15b1368cb56214879e75b24b33a31791
utumdol <utumdol@gmail.com>
2012-01-12 15:23:43 +0900

    amend get_default

--- vendor/phpworks/functions/form.php
+++ vendor/phpworks/functions/form.php
@@ -4,8 +4,17 @@
  * get_default($obj, $prop1, $prop2, ...) means $obj->prop1->prop2.
  * get_default($arr, $idx1, $idx2, ...) means $arr[$idx1][$idx2].
  */
-#function get_default($obj, $props = array()) {
 function get_default(/* $obj, $prop1, $prop2, ... or $arr, $idx1, $idx2, ...*/) {
+	$args = func_get_args();
+	if (empty($args)) {
+		return null;
+	}
+
+	$props = array_slice($args, 1);
+	if (empty($props)) {
+		return $args;
+	}
+
 	// return old value first.
 	$flash = Context::one()->flash;
 	$old_params = get_array_value($flash->get('old_params'), $props);
@@ -13,16 +22,14 @@ function get_default(/* $obj, $prop1, $prop2, ... or $arr, $idx1, $idx2, ...*/) 
 		return $old_params;
 	}
 
-	$args = func_get_args();
-	if (empty($args)) {
-		return null;
-	}
 	if (is_array($args[0])) {
 		return get_array_value($args[0], array_slice($args, 1));
 	}
+
 	if (is_object($args[0])) {
 		return get_object_property($args[0], array_slice($args, 1));
 	}
+
 	return null;
 }
 
