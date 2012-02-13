127b5749beafa77dd620b6e56e0566ba8d9d5df0
utumdol <utumdol@gmail.com>
2012-02-13 17:11:28 +0900

    add layout generator

---
+++ vendor/phpworks/classes/generator/layout_generator.class.php
@@ -0,0 +1,33 @@
+<?php
+class LayoutGenerator extends Generator {
+	public $path;
+	public $template = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="/stylesheets/default.css" media="all" rel="stylesheet" type="text/css" />
+<script src="/javascripts/jquery.js" type="text/javascript"></script>
+<title><class></title>
+</head>
+<body>
+<div id="contents">
+	<?= $CONTENTS ?>
+</div>
+</div>
+</body>
+</html>
+
+';
+
+	public function __construct($name, $from = '') {
+		parent::__construct($name);
+		$this->path = Config::get('layout_dir');
+	}
+
+	public function get_contents() {
+		$class = filename_to_classname($this->name);
+		$result = str_replace('<class>', $class, $this->template);
+		return $result;
+	}
+}
+
