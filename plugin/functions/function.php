08a3213454f6eff67bdadbb24b6829bb39a72a94
utumdol <utumdol@gmail.com>
2013-08-09 16:16:47 +0900

    rename parse_array_args to uncover_array

--- plugin/functions/function.php
+++ plugin/functions/function.php
@@ -3,10 +3,10 @@
  * 배열 안에 배열이 있다면 해당 배열을 반환한다.
  * 아니라면 자신을 반환한다.
  */
-function parse_array_args($args = array()) {
-	if (sizeof($args) == 1 && is_array($args[0])) {
-		return $args[0];
+function uncover_array($arr = array()) {
+	if (sizeof($arr) == 1 && is_array($arr[0])) {
+		return $arr[0];
 	}
-	return $args;
+	return $arr;
 }
 
--- plugin/functions/global.php
+++ plugin/functions/global.php
@@ -3,28 +3,28 @@
  * alias of $_SERVER[arg1][arg2][arg3][...]
  */
 function _server(/* arg1, arg2, arg3, ... */) {
-	return get_array_value($_SERVER, parse_array_args(func_get_args()));
+	return get_array_value($_SERVER, uncover_array(func_get_args()));
 }
 
 /**
  * alias of $_GET[arg1][arg2][arg3][...]
  */
 function _get(/* arg1, arg2, arg3, ... */) {
-	return get_array_value($_GET, parse_array_args(func_get_args()));
+	return get_array_value($_GET, uncover_array(func_get_args()));
 }
 
 /**
  * alias of $_POST[arg1][arg2][arg3][...]
  */
 function _post(/* arg1, arg2, arg3, ... */) {
-	return get_array_value($_POST, parse_array_args(func_get_args()));
+	return get_array_value($_POST, uncover_array(func_get_args()));
 }
 
 /**
  * alias of $_FILES[arg1][arg2][arg3][...]
  */
 function _files(/* arg1, arg2, arg3, ... */) {
-	return get_array_value($_FILES, parse_array_args(func_get_args()));
+	return get_array_value($_FILES, uncover_array(func_get_args()));
 }
 
 /**
@@ -36,7 +36,7 @@ function _cookie(/* key[, value] */) {
 		$_COOKIE[$args[0]] = $args[1];
 		return;
 	}
-	return get_array_value($_COOKIE, parse_array_args(func_get_args()));
+	return get_array_value($_COOKIE, uncover_array(func_get_args()));
 }
 
 /**
@@ -48,13 +48,13 @@ function _session(/* key[, value] */) {
 		$_SESSION[$args[0]] = $args[1];
 		return;
 	}
-	return get_array_value($_SESSION, parse_array_args(func_get_args()));
+	return get_array_value($_SESSION, uncover_array(func_get_args()));
 }
 
 /**
  * alias of $_REQUEST[arg1][arg2][arg3][...]
  */
 function _request(/* arg1, arg2, arg3, ... */) {
-	return get_array_value($_REQUEST, parse_array_args(func_get_args()));
+	return get_array_value($_REQUEST, uncover_array(func_get_args()));
 }
 
